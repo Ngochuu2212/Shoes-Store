7544c8effa846469d8154b22c27ae771c4653755
Ngochuu2212 <phamngochuu3101@gmail.com>
2026-07-03 10:32:07 +0700

    commit 5

--- API_Shoes_Management/src/providers/EmailProvider.js
+++ API_Shoes_Management/src/providers/EmailProvider.js
@@ -1,29 +1,27 @@
-import axios from 'axios'
+import nodemailer from 'nodemailer'
 import { env } from '~/config/environment'
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: env.BREVO_SMTP_USER, // Tận dụng biến môi trường hiện tại
+    pass: env.BREVO_API_KEY    // để không cần đổi tên biến trên Render
+  }
+})
 
 const sendEmail = async (to, subject, htmlContent) => {
   try {
-    const response = await axios.post(
-      'https://api.brevo.com/v3/smtp/email',
-      {
-        sender: {
-          email: env.BREVO_SMTP_USER,
-          name: 'Shoes Store'
-        },
-        to: [{ email: to }],
-        subject: subject,
-        htmlContent: htmlContent
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'api-key': env.BREVO_API_KEY
-        }
-      }
-    )
-    return response.data
+    const mailOptions = {
+      from: `"Shoes Store" <${env.BREVO_SMTP_USER}>`,
+      to: to,
+      subject: subject,
+      html: htmlContent
+    }
+
+    const info = await transporter.sendMail(mailOptions)
+    return info
   } catch (error) {
-    throw new Error(`Lỗi gửi Email: ${error.response?.data?.message || error.message}`)
+    throw new Error(`Lỗi gửi Email: ${error.message}`)
   }
 }
 
