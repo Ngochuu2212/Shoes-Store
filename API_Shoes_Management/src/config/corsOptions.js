944a263616fb2c90c9027e241d57dc79817d1b50
Ngochuu2212 <phamngochuu3101@gmail.com>
2026-07-03 09:33:02 +0700

    commit 3

--- API_Shoes_Management/src/config/corsOptions.js
+++ API_Shoes_Management/src/config/corsOptions.js
@@ -1,14 +1,22 @@
+import { env } from './environment'
+
 export const corsOptions = {
   origin: function (origin, callback) {
     // Danh sách các origin được phép
     const allowedOrigins = [
       'http://localhost:5173',
       'https://shoes-ecommerce-platform.vercel.app'
-      // Thêm các domain khác nếu cần
     ]
 
-    // Nếu không có origin (ví dụ: Postman, curl) hoặc origin được phép
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (env.FRONTEND_URL) {
+      allowedOrigins.push(env.FRONTEND_URL)
+    }
+
+    // Cho phép nếu không có origin (như Postman) hoặc nằm trong danh sách
+    // Hoặc là bất kỳ tên miền nào từ Vercel (*.vercel.app)
+    const isVercelDomain = origin && origin.endsWith('.vercel.app')
+
+    if (!origin || allowedOrigins.includes(origin) || isVercelDomain) {
       return callback(null, true)
     } else {
       return callback(new Error('Not allowed by CORS'), false)
