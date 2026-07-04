fcbe36708897e3e035b45ca90c6b21fa7bbd97c8
Ngochuu2212 <phamngochuu3101@gmail.com>
2026-07-04 15:56:53 +0700

    style: move AI chatbot button upward to prevent overlap with vendor chat icon

--- WEB_Shoes_Management/src/components/common/AIChatbot.jsx
+++ WEB_Shoes_Management/src/components/common/AIChatbot.jsx
@@ -68,7 +68,7 @@ export const AIChatbot = () => {
   }
 
   return (
-    <div className="fixed bottom-6 right-6 z-40 select-none">
+    <div className="fixed bottom-24 right-6 z-40 select-none">
       {/* Bong bóng Chat nổi */}
       <motion.button
         whileHover={{ scale: 1.08, y: -2 }}
